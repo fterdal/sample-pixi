d21d0140edf4227a30b5df02abfd5964659eeeba
Finn <finnterdal@gmail.com>
2019-11-25 16:36:10 -0600

    got me some pixi

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-import * as PIXI from 'pixi'
+import * as PIXI from 'pixi.js'
 
 console.log(PIXI)
+
+let app = new PIXI.Application()
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view)
